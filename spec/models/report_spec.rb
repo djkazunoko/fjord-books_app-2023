f078c6e53abedcbfebf6d8c26dd0515cadacbb78
djkazunoko <djkazunoko@gmail.com>
2024-01-20 22:00:22 +0900

    Reportのモデルスペックでletを使ってDRYに

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -3,30 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe Report, type: :model do
+  let(:owner) { FactoryBot.create(:user) }
+  let(:other_user) { FactoryBot.create(:user) }
+  let(:report) { FactoryBot.create(:report, user: owner) }
+  let(:mentioning_report) { FactoryBot.create(:report, content: "http://localhost:3000/reports/#{report.id}") }
+
   it 'has a valid factory' do
-    expect(FactoryBot.build(:report)).to be_valid
+    expect(report).to be_valid
   end
 
   describe '#editable?' do
     it 'is editable by the owner' do
-      report = FactoryBot.build(:report)
-      owner = report.user
-
       expect(report.editable?(owner)).to eq true
     end
 
     it 'is not editable by the other user' do
-      report = FactoryBot.build(:report)
-      other_user = FactoryBot.build(:user)
-
       expect(report.editable?(other_user)).to eq false
     end
   end
 
   describe '#created_on' do
     it 'returns the created date' do
-      report = FactoryBot.build(:report)
-
       report.created_at = Time.zone.parse('2024/1/19')
 
       expect(report.created_on).to eq Date.new(2024, 1, 19)
@@ -36,25 +33,19 @@ RSpec.describe Report, type: :model do
   describe '#save_mentions' do
     context 'when the mentioning report is created' do
       it 'can mention other reports' do
-        mentioned_report = FactoryBot.create(:report)
-        mentioning_report = FactoryBot.create(:report, content: "http://localhost:3000/reports/#{mentioned_report.id}")
-
-        expect(mentioned_report.mentioned_reports).to include mentioning_report
+        expect(report.mentioned_reports).to include mentioning_report
       end
     end
 
     context 'when the mentioning report is updated' do
       it 'updates mentions to other reports' do
-        mentioned_report = FactoryBot.create(:report)
-        mentioning_report = FactoryBot.create(:report, content: "http://localhost:3000/reports/#{mentioned_report.id}")
         mentioning_report.content = 'no mention'
         mentioning_report.save
 
-        expect(mentioned_report.mentioned_reports).to_not include mentioning_report
+        expect(report.mentioned_reports).to_not include mentioning_report
       end
 
       it 'can not mention its own report' do
-        report = FactoryBot.create(:report)
         report.content = "http://localhost:3000/reports/#{report.id}"
         report.save
 
@@ -64,11 +55,9 @@ RSpec.describe Report, type: :model do
 
     context 'when the mentioning report is deleted' do
       it 'deletes mentions to other reports' do
-        mentioned_report = FactoryBot.create(:report)
-        mentioning_report = FactoryBot.create(:report, content: "http://localhost:3000/reports/#{mentioned_report.id}")
         mentioning_report.destroy
 
-        expect(mentioned_report.mentioned_reports).to be_empty
+        expect(report.mentioned_reports).to be_empty
       end
     end
   end
