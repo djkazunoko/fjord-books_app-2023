b0cae5ccf5642f1fde792ab68e71dd9026fcc264
djkazunoko <djkazunoko@gmail.com>
2024-01-20 15:27:57 +0900

    日報削除のシステムスペックを作成
    全てのテストを通しで行うとreportが連番で作成されて数字がずれるため

--- spec/system/reports_spec.rb
+++ spec/system/reports_spec.rb
@@ -40,7 +40,6 @@ RSpec.describe 'Reports', type: :system do
     click_link '日報'
 
     click_link 'この日報を表示'
-    expect(page).to have_content 'A test report1.'
 
     click_link 'この日報を編集'
     fill_in 'タイトル', with: '日報1'
@@ -50,5 +49,26 @@ RSpec.describe 'Reports', type: :system do
     expect(page).to have_content '日報が更新されました。'
     expect(page).to have_content 'タイトル: 日報1'
     expect(page).to have_content '内容: 書き換えました。'
+  end
+
+  scenario 'user deletes a report' do
+    report = FactoryBot.create(:report)
+    user = report.user
+
+    visit root_path
+    fill_in 'Eメール', with: user.email
+    fill_in 'パスワード', with: user.password
+    click_button 'ログイン'
+
+    expect(page).to have_content 'ログインしました。'
+
+    click_link '日報'
+
+    click_link 'この日報を表示'
+
+    expect {
+      click_button 'この日報を削除'
+      expect(page).to have_content '日報が削除されました。'
+    }.to change(user.reports, :count).by(-1)
   end
 end
